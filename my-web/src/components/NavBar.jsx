fb32b674825132342714633b0f876eb98ce967d0
Orlando Mata Monge <orl1289@gmail.com>
2024-02-16 10:56:34 +0100

    Inserted animation into Nav bar; also fixed the component

--- my-web/src/components/NavBar.jsx
+++ my-web/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Lottie from 'lottie-react'
+import animationData from "../assets/bg-logo.json"
 
 function Navbar() {
     return (
@@ -8,12 +9,20 @@ function Navbar() {
       <nav className='bg-blue-900 sticky top-0 left-0 right-0 w-full'>
         <div className="container mx-auto px-4">
           <div className="flex justify-between items-center py-4">
-            <div className="flex-shrink-0">
-              <img className="h-8" src="/path/to/logo.png" alt="Logo" />
-              <h1>
-	<span class="font-bold bg-gradient-to-br from-blue-700 to-green-400 bg-clip-text text-transparent box-decoration-clone">OMM</span>
-	</h1>
+
+            {/*  Inserting the animation*/}
+            <div className="inline-flex">
+        
+              <Lottie animationData={animationData}
+                loop={1}
+                autoplay={true}
+                style={{ width: 90, height: 90 }}/>
+              <p class=" text-7xl font-bold bg-gradient-to-br from-blue-700 to-green-400 bg-clip-text text-transparent">
+	            OMM
+	           </p>
             </div>
+
+
             <div className="hidden md:block">
               <ul className="flex space-x-6">
                 <button class='bg-blue-900 rounded-full w-30 border-white'>
