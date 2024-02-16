08564a8096af5568a9b9fca433967e6f0c5baa53
Orlando Mata Monge <orl1289@gmail.com>
2024-02-16 10:11:46 +0100

    Fixed the buttons on the Nav Bar

--- my-web/src/components/NavBar.jsx
+++ my-web/src/components/NavBar.jsx
@@ -10,16 +10,19 @@ function Navbar() {
           <div className="flex justify-between items-center py-4">
             <div className="flex-shrink-0">
               <img className="h-8" src="/path/to/logo.png" alt="Logo" />
+              <h1>
+	<span class="font-bold bg-gradient-to-br from-blue-700 to-green-400 bg-clip-text text-transparent box-decoration-clone">OMM</span>
+	</h1>
             </div>
             <div className="hidden md:block">
               <ul className="flex space-x-6">
-                <button class='bg-blue-900 rounded-full w-20 border-white'>
+                <button class='bg-blue-900 rounded-full w-30 border-white'>
                     <a href="#" className="text-white hover:text-orange-500">CV</a>
                 </button>
-                <button class='bg-blue-900 rounded-full w-20 border-white text-center'>
+                <button class='bg-blue-900 rounded-full w-30 border-white text-center'>
                     <a href="#" className="text-white hover:text-orange-500">Resume</a>
                 </button>
-                <button class='bg-blue-900 rounded-full w-20 border-white'>
+                <button class='bg-blue-900 rounded-full w-30 border-white'>
                     <a href="#" className="text-white hover:text-orange-500">Blog</a>
                 </button>
               </ul>
