7de4affc94e08546b309dc42ce0ad3d685095ae2
Orlando Mata Monge <orl1289@gmail.com>
2024-02-19 10:12:04 +0100

    Modified the Tailwind breakpoints for tablet, laptop, desktop and fixed the NavBar responsiveness

--- my-web/src/components/NavBar.jsx
+++ my-web/src/components/NavBar.jsx
@@ -12,30 +12,31 @@ function Navbar() {
           <div className="flex justify-between items-center py-4">
 
             {/*  Inserting the animation*/}
-            <div className="inline-flex">
+            <div className="grid items-center tablet:inline-flex">
         
-              <Lottie animationData={animationData}
+              <Lottie className='h-auto max-w-[5rem] tablet:max-w-[8rem] laptop:max-w-[10rem] desktop:max-w-[13rem]' animationData={animationData}
                 loop={1}
                 autoplay={true}
-                style={{ width: 90, height: 90 }}/>
-              <p class=" text-7xl font-bold bg-gradient-to-br from-blue-700 to-green-400 bg-clip-text text-transparent">
+                />
+            
+              <p class=" text-2xl tablet:text-4xl laptop:text-6xl desktop:text-8xl font-bold bg-gradient-to-br from-blue-700 to-green-400 bg-clip-text text-transparent">
 	            OMM
 	           </p>
             </div>
 
 
             <div>
-              <ul className="flex space-x-6">
+              <ul className="flex space-x-6 desktop:">
                 
                 <Link to="/" className="text-white hover:text-orange-500">
-                <button class='bg-blue-900 rounded-full w-30 border-white'>CV</button>
+                <button class='text-xs tablet:text-sm laptop:text-base desktop:text-lg bg-blue-900 rounded-full w-30 border-white'>CV</button>
                 </Link>
                 
                 <Link to="/resume" className="text-white hover:text-orange-500">
-                <button class='bg-blue-900 rounded-full w-30 border-white'>Resume</button>
+                <button class='text-xs tablet:text-sm laptop:text-base desktop:text-lg bg-blue-900 rounded-full w-30 border-white'>Resume</button>
                 </Link>
                 <Link to="/blog" className="text-white hover:text-orange-500">
-                <button class='bg-blue-900 rounded-full w-30 border-white'>Blog</button>
+                <button class='text-xs tablet:text-sm laptop:text-base desktop:text-lg bg-blue-900 rounded-full w-30 border-white'>Blog</button>
                 </Link>
               </ul>
             </div>
