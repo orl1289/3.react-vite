737bff99a1cb0036e348ddc25bcc71b99c2a0592
Orlando Mata Monge <orl1289@gmail.com>
2024-02-24 13:31:00 +0100

    Fixed the issue with the footer when clicked on and sending to incorrect location

--- my-web/src/components/Footer.jsx
+++ my-web/src/components/Footer.jsx
@@ -1,46 +1,52 @@
-import React from 'react';
-import Lottie from 'lottie-react'
-import animationData from "../assets/bg-logo.json"
-import { Link } from 'react-router-dom'; // Import Link from React Router
+import React from "react";
+import Lottie from "lottie-react";
+import animationData from "../assets/bg-logo.json";
 
 function Navbar() {
-    return (
-       
-    
-      <nav className='bg-blue-900 sticky top-0 left-0 right-0 w-full'>
-        <div className=" mx-auto px-4">
-          <div className="flex justify-between items-center py-4">
+  return (
+    <nav className="bg-blue-900 sticky top-0 left-0 right-0 w-full">
+      <div className=" mx-auto px-4">
+        <div className="flex justify-between items-center py-4">
+          {/*  Inserting the animation*/}
+          <div className="inline-grid">
+            <Lottie
+              className="h-auto max-w-[5rem] tablet:max-w-[8rem] laptop:max-w-[10rem] desktop:max-w-[13rem]"
+              animationData={animationData}
+              loop={1}
+              autoplay={true}
+            />
 
-            {/*  Inserting the animation*/}
-            <div className="inline-grid">
-        
-            <Lottie className='h-auto max-w-[5rem] tablet:max-w-[8rem] laptop:max-w-[10rem] desktop:max-w-[13rem]' animationData={animationData}
-                loop={1}
-                autoplay={true}
-                />
+            <p className="text-sm text-white tablet:text-base laptop:text-lg desktop:text-xl">
+              {" "}
+              Todos los derechos reservados
+            </p>
+          </div>
 
-              <p className='text-sm text-white tablet:text-base laptop:text-lg desktop:text-xl'> Todos los derechos reservados</p>
-            </div>
-
-
-            <div className="md:block">
-              <ul className="flex space-x-6">
-              <a href="/" className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl">
-                CV
+          <div className="md:block">
+            <ul className="flex space-x-6">
+              <a
+                href="/"
+                className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl"
+              >
+                Español
               </a>
-              <a href="/resume" className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl">
-               Resume
+              <a
+                href="/english"
+                className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl"
+              >
+                English
               </a>
-              <a href="/blog" className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl">
+              <a
+                href="/blog"
+                className="text-sm text-white hover:text-orange-500 tablet:text-base laptop:text-lg desktop:text-xl"
+              >
                 Blog
               </a>
-
-              </ul>
-            </div>
+            </ul>
           </div>
         </div>
-      </nav>
-   
-    );
-  }
+      </div>
+    </nav>
+  );
+}
 export default Navbar;
